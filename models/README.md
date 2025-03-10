cdab4d3b2123b56d84210319cafb2f184070ad93
Eugene Vuong <vuongeugene@gmail.com>
2025-03-09 22:06:28 -0700

    Implemented Interface for Groq

---
+++ models/README.md
@@ -0,0 +1,7 @@
+i am lazy to make my readme look good
+example:
+
+```
+model = GoogleGeminiModel(model_name="bruh", api_key="bruh")
+model.invoke(messages=["bruh!", "bruh?"])
+```
---
+++ models/groq.py
@@ -0,0 +1,26 @@
+from base_model import BaseModel
+from groq import Groq
+
+class BaseModel:
+    def __init__(self, model_name, api_key):
+        self.model_name = model_name
+        self.client = Groq(api_key=api_key)
+
+    def invoke(self, message, multimodel_input=None):
+        if multimodel_input is not None:
+            raise NotImplementedError("Multimodal input is not supported for this model")
+
+        formatted_messages = []
+        for msg in message:
+            formatted_messages.append({
+            "role": "user",
+            "content": msg
+            })
+        chat_completion = self.client.chat.completions.create(
+            messages=formatted_messages,
+            model=self.model_name,
+        )
+        return chat_completion.choices[0].message.content
+
+
+        
