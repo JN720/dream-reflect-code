150425acbd7241f2c31af144f0a817874f22d029
Eugene Vuong <vuongeugene@gmail.com>
2025-03-09 22:14:31 -0700

    Implemented Interface for Groq

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from google_gemini import GoogleGeminiModel
+from groq import GroqModel
+
+# Export these classes for easier access when importing from models package
+__all__ = ['GoogleGeminiModel', 'GroqModel']
