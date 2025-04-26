b9adb91003c5bad7c5f8a40a5a63ae9c4c599c5c
JN720 <jsnxyz695@gmail.com>
2025-04-25 21:12:48 -0700

    added cartpole world model

--- world_models.py
+++ world_models.py
@@ -13,3 +13,17 @@ class LunarLanderWorldModel(nn.Module):
 
     def forward(self, x):
         return self.network(x)
+
+class CartpoleWorldModel(nn.Module):
+    def __init__(self):
+        super(CartpoleWorldModel, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(4 + 2, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 4 + 1)
+        )
+
+    def forward(self, x):
+        return self.network(x)
